1a31f395837d3801c40481165556f1be9ec3d8ba
ZclipseCode <bmichaelbusiness@gmail.com>
2022-06-10 15:11:46 -0500

    Goblin Hit and Death Animations as Well as THE FADE

--- Vitalite/Assets/Scripts/Goblin.cs
+++ Vitalite/Assets/Scripts/Goblin.cs
@@ -7,15 +7,24 @@ public class Goblin : MonoBehaviour
     //animation
     private SpriteRenderer renderer2d;
     private Animator animator;
+    private float hitAnimTime;
+    private bool isFading;
+    private bool fadeActivated;
 
     //health
     public float health;
+    private float tempHealth;
 
     void Start()
     {
         //animation
         renderer2d = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        isFading = false;
+        fadeActivated = false;
+
+        //health
+        tempHealth = health;
     }
 
     void Update()
@@ -28,9 +37,30 @@ public class Goblin : MonoBehaviour
             animator.SetInteger("AnimState", 0);
         }
 
-        if (health <= 0)
+        if (tempHealth != health && health <= 0)
         {
-            Destroy(gameObject);
+            hitAnimTime += Time.deltaTime;
+            animator.SetInteger("AnimState", 4);
+            isFading = true;
+            if (isFading && !fadeActivated)
+            {
+                startFading();
+                fadeActivated = true;
+            }
+            if (hitAnimTime >= 1f)
+            {
+                Destroy(gameObject);
+            }
+            
+        } else if (tempHealth != health)
+        {
+            hitAnimTime += Time.deltaTime;
+            animator.SetInteger("AnimState", 3);
+            if (hitAnimTime >= 0.4f)
+            {
+                hitAnimTime = 0f;
+                tempHealth = health;
+            }
         }
     }
 
@@ -38,5 +68,22 @@ public class Goblin : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+    }
+
+    //fade-out
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= 0.05f; f -= 0.05f)
+        {
+            Color c = renderer2d.material.color;
+            c.a = f;
+            renderer2d.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public void startFading()
+    {
+        StartCoroutine("FadeOut");
     }
 }
